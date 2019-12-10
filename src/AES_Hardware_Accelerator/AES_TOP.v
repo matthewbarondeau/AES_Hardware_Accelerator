9bd321c719091a7e5904dfcc344bd5955e1c28e3
Matthew Barondeau <mebarondeau@utexas.edu>
2019-12-09 18:48:32 -0600

    Move block to BRAM

--- src/AES_Hardware_Accelerator/AES_TOP.v
+++ src/AES_Hardware_Accelerator/AES_TOP.v
@@ -34,55 +34,44 @@ module AES_TOP(
 	output reg			aes_start_read,
 	output reg	[31:0]	aes_bram_addr,
 	input  wire	[31:0]	aes_bram_read_data,
-	output wire [4:0]   aes_state,
-	input  wire [255:0] aes_key_input,
-	input  wire [127:0] aes_block_input
+  
+  input  wire [255:0] aes_key_input1,
+  input  wire [255:0] aes_key_input2
+  
   );
-
 
 // --------------------[  WIRES and REGISTERS  ]-----------------------------------
 //              
 
-	wire 			aes_idle;
-	wire [127:0]	aes_result;
-	reg				first_chunk;
-	reg				next_chunk;
-	reg				aes_data_valid;
-	reg				aes_core_rst_n;
+	wire 			    aes_idle;
+	wire  [127:0] aes_result;
+	reg				    first_chunk;
+	reg				    next_chunk;
+	reg				    aes_data_valid;
+	reg				    aes_core_rst_n;
+		
+	reg	  [31:0]	aes_bram_addr_nxt;
+	reg	  [4:0]		STATE;
+	reg	  [4:0]		NXT_STATE;	
+
+	reg	  [31:0]	block_reg_core1	[0:3];
+	wire  [127:0] core1_block;
+
+  assign core1_block  	= {	block_reg_core1[0], block_reg_core1[1],
+                            block_reg_core1[2], block_reg_core1[3]};
 	
-	reg	[31:0]		aes_chunk_ctr;
-	reg	[31:0]		aes_chunk_ctr_nxt;
-	
-	reg	[31:0]		aes_bram_addr_nxt;
-	reg	[4:0]		STATE;
-	reg	[4:0]		NXT_STATE;	
-
-	reg	[31:0]	block_reg	[0:11];
-	//reg	[31:0]	key_reg		[0:7];
-	wire[255:0] core_key;
-	wire[127:0] core_block;
-
-  	assign core_key 	= {	block_reg[0], block_reg[1], block_reg[2], block_reg[3],
-                     		block_reg[4], block_reg[5], block_reg[6], block_reg[7]};
-
-
-  	assign core_block  	= {	block_reg[8], block_reg[9],
-                        	block_reg[10], block_reg[11]};
-                        	
-    assign aes_state    = STATE;
-
-	localparam
-			INIT		= 0,
-			AES_READ1	= 1,
-			AES_READ2	= 2,
-			AES_READ3	= 3,
-			START_AES	= 4,
-			WAIT_AES	= 5,
+  localparam
+			INIT		    = 0,
+			AES_READ1	  = 1,
+			AES_READ2	  = 2,
+			AES_READ3	  = 3,
+			START_AES	  = 4,
+			WAIT_AES	  = 5,
 			START_AES2  = 6,
 			WAIT_AES2   = 7,
-			LOOP_AES	= 8,
-      WAIT_AES3 = 10,
-			HOLD		= 9;
+			LOOP_AES	  = 8,
+      WAIT_AES3   = 10,
+			HOLD		    = 9;
 
 
 	// AES State Machine
@@ -94,33 +83,22 @@ module AES_TOP(
 	begin : reg_reset
 	integer i;
 		if(aes_rst_n == 1'b0) begin
-			block_reg[0]		<= 32'h0;
-			block_reg[1] 		<= 32'h0;
-			block_reg[2]		<= 32'h0;
-			block_reg[3]		<= 32'h0;
-			block_reg[4]		<= 32'h0;
-			block_reg[5]		<= 32'h0;
-			block_reg[6]		<= 32'h0;
-			block_reg[7]		<= 32'h0;
-			block_reg[8]		<= 32'h0;
-			block_reg[9]		<= 32'h0;
-			block_reg[10]		<= 32'h0;
-			block_reg[11]		<= 32'h0;
+			block_reg_core1[0]		<= 32'h0;
+			block_reg_core1[1] 		<= 32'h0;
+			block_reg_core1[2]		<= 32'h0;
+			block_reg_core1[3]		<= 32'h0;
 			
 			first_chunk			<= 1'b0;
 			next_chunk			<= 1'b0;
-			aes_chunk_ctr		<= 32'h0;
 			aes_bram_addr		<= 32'h0;
-			aes_data_valid		<= 1'h0;
+			aes_data_valid  <= 1'h0;
 			reg_num_nxt			<= 4'h0;
-			aes_start_read		<= 1'b0;
+			aes_start_read	<= 1'b0;
 			aes_complete		<= 1'b0;			
-			NXT_STATE			<= INIT;			
-
+			NXT_STATE			  <= INIT;			
 		end else if(aes_rst_n == 1'b1) begin
 			if((STATE == INIT) && (axi_start_aes == 1'b1)) begin
 				aes_bram_addr 	<= aes_bram_addr_start;
-				aes_chunk_ctr 	<= aes_num_chunks;
 				aes_complete  	<= 1'b0;
 				NXT_STATE		<= AES_READ1;
 			end else if((STATE == INIT) && (axi_start_aes == 1'b0)) begin
@@ -133,12 +111,12 @@ module AES_TOP(
 				NXT_STATE <= AES_READ2;
 			end else if((STATE == AES_READ2) && (bram_complete)) begin
 				aes_start_read <= 1'b0;
-				block_reg[reg_num] <= aes_bram_read_data;
+				block_reg_core1[reg_num] <= aes_bram_read_data;
 				NXT_STATE <= AES_READ3;
-			end else if((STATE == AES_READ3) && (reg_num[3:0] == 4'b1011)) begin
+			end else if((STATE == AES_READ3) && (reg_num[3:0] == 4'b0011)) begin
 				reg_num_nxt	<= 4'b0000;
 				NXT_STATE <= START_AES;
-			end else if((STATE == AES_READ3) && (reg_num[3:0] != 4'b1011)) begin
+			end else if((STATE == AES_READ3) && (reg_num[3:0] != 4'b0011)) begin
 				reg_num_nxt	<= reg_num + 4'b0001;
 				aes_bram_addr	<= aes_bram_addr_nxt + 32'h4;
 				aes_start_read	<= 1'b1;
@@ -180,11 +158,9 @@ module AES_TOP(
 		if(aes_rst_n == 1'b0) begin
 			reg_num <= 4'b0;
 			aes_bram_addr_nxt <= aes_bram_addr;
-			aes_chunk_ctr_nxt <= aes_chunk_ctr;
 			STATE <= INIT;
 		end else if(aes_rst_n == 1'b1) begin
 			reg_num 	<= reg_num_nxt;
-			aes_chunk_ctr_nxt <= aes_chunk_ctr;
 			aes_bram_addr_nxt <= aes_bram_addr;
 			STATE <= NXT_STATE;
 		end
@@ -204,10 +180,10 @@ module AES_TOP(
 		.next(next_chunk),
 		.ready(aes_idle),
 		
-		.key(aes_key_input),
+		.key(aes_key_input1),
 		.keylen(1'b1),	// 1 for 256 bit, 0 for 128
 		
-		.block(aes_block_input),
+		.block(core1_block),
 		.result(aes_result),
 		.result_valid(aes_digest_valid)
 	);
--- src/AES_Hardware_Accelerator/aes_accelerator_v1_0_S00_AXI.v
+++ src/AES_Hardware_Accelerator/aes_accelerator_v1_0_S00_AXI.v
@@ -113,11 +113,8 @@
 	wire		aes_digest_valid;
 	wire		aes_complete;
 	wire   [31:0]  aes_bram_addr;
-	wire   [4:0]   aes_state;
-	wire   [255:0] aes_key;
-	wire   [127:0] aes_block;
-
-
+	wire   [255:0] aes_key_core1;
+  wire   [255:0] aes_key_core2;
 
 
 	// AXI4LITE signals
@@ -707,23 +704,23 @@
 	        5'h09   : reg_data_out <= aes_digest_reg[95:64];
 	        5'h0A   : reg_data_out <= aes_digest_reg[63:32];
 	        5'h0B   : reg_data_out <= aes_digest_reg[31:0];
-	        5'h0C   : reg_data_out <= aes_state;
-	        5'h0D   : reg_data_out <= aes_key[255:224];
-	        5'h0E   : reg_data_out <= aes_key[223:192];
-	        5'h0F   : reg_data_out <= aes_key[191:160];
-	        5'h10   : reg_data_out <= aes_key[159:128];
-	        5'h11   : reg_data_out <= aes_key[127:96];
-	        5'h12   : reg_data_out <= aes_key[95:64];
-	        5'h13   : reg_data_out <= aes_key[63:32];
-	        5'h14   : reg_data_out <= aes_key[31:0];
-	        5'h15   : reg_data_out <= aes_block[127:96];
-	        5'h16   : reg_data_out <= aes_block[95:64];
-	        5'h17   : reg_data_out <= aes_block[63:32];
-	        5'h18   : reg_data_out <= aes_block[31:0];
-	        5'h19   : reg_data_out <= slv_reg25;
-	        5'h1A   : reg_data_out <= slv_reg26;
-	        5'h1B   : reg_data_out <= slv_reg27;
-	        5'h1C   : reg_data_out <= slv_reg28;
+	        5'h0C   : reg_data_out <= 32'hbeefface;
+	        5'h0D   : reg_data_out <= aes_key_core1[255:224];
+	        5'h0E   : reg_data_out <= aes_key_core1[223:192];
+	        5'h0F   : reg_data_out <= aes_key_core1[191:160];
+	        5'h10   : reg_data_out <= aes_key_core1[159:128];
+	        5'h11   : reg_data_out <= aes_key_core1[127:96];
+	        5'h12   : reg_data_out <= aes_key_core1[95:64];
+	        5'h13   : reg_data_out <= aes_key_core1[63:32];
+	        5'h14   : reg_data_out <= aes_key_core1[31:0];
+	        5'h15   : reg_data_out <= aes_key_core2[255:224];
+	        5'h16   : reg_data_out <= aes_key_core2[223:192];
+	        5'h17   : reg_data_out <= aes_key_core2[191:160];
+	        5'h18   : reg_data_out <= aes_key_core2[159:128];
+	        5'h19   : reg_data_out <= aes_key_core2[127:96];
+	        5'h1A   : reg_data_out <= aes_key_core2[95:64];
+	        5'h1B   : reg_data_out <= aes_key_core2[63:32];
+	        5'h1C   : reg_data_out <= aes_key_core2[31:0];
 	        5'h1D   : reg_data_out <= slv_reg29;
 	        5'h1E   : reg_data_out <= slv_reg30;
 	        5'h1F   : reg_data_out <= slv_reg31;
@@ -753,10 +750,12 @@
 	// Add user logic here
 	
 		
-    assign aes_key     = { slv_reg13, slv_reg14, slv_reg15, slv_reg16,
-                           slv_reg17, slv_reg18, slv_reg19, slv_reg20};
-                           
-    assign aes_block   = { slv_reg21, slv_reg22, slv_reg23, slv_reg24};
+    assign aes_key_core1  = { slv_reg13, slv_reg14, slv_reg15, slv_reg16,
+                              slv_reg17, slv_reg18, slv_reg19, slv_reg20};
+                        
+    assign aes_key_core2  = { slv_reg21, slv_reg22, slv_reg23, slv_reg24,
+                              slv_reg25, slv_reg26, slv_reg27, slv_reg28};
+                        
                            
                            
     always @( posedge S_AXI_ACLK)
@@ -819,10 +818,10 @@
         
         .aes_start_read(aes_start_read),
         .aes_bram_addr(aes_bram_addr),
-        .aes_bram_read_data(aes_bram_read_data),
-        .aes_state(aes_state),
-        .aes_key_input(aes_key),
-        .aes_block_input(aes_block)
+        .aes_bram_read_data(aes_bram_read_data),        
+        
+        .aes_key_input1(aes_key_core1),
+        .aes_key_input2(aes_key_core2)
          );
                     
 
